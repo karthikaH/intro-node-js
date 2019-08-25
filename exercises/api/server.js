a1b3793d5055c6569e51b1d34bf39eef05533186
karthikaH <hariharan.karthika93@gmail.com>
2019-08-25 19:45:33 +0530

    added changes to server.js file

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -8,8 +8,16 @@ const path = require('path')
  * @param {String} name full file name of asset in asset folder
  */
 const findAsset = (name) => {
-  const assetPath = path.join(__dirname, 'assets', name)
-  return fs.readFileSync(assetPath, {encoding: 'utf-8'}).toString()
+  const assetPath = path.join(__dirname, 'assets', name);
+  return new Promise((resolve, reject)=> {
+    fs.readFile(assetPath, {encoding: 'utf-8'}, (err, result)=>{
+      if(err){
+        reject(err);
+      } else {
+        resolve(result.toString())
+      }
+    })
+  })
 }
 
 const hostname = '127.0.0.1'
@@ -18,20 +26,36 @@ const port = 3000
 // log incoming request coming into the server. Helpful for debugging and tracking
 const logRequest = (method, route, status) => console.log(method, route, status)
 
-const server = http.createServer((req, res) => {
+const router = {
+  //route Method
+  '/ GET': {
+    asset: 'index.html',
+    mime: 'text/html',
+  },
+  '/style.css GET': {
+    asset: 'style.css',
+    mime: 'text/css',
+  }
+}
+
+const server = http.createServer(async (req, res) => {
+
   const method = req.method
   const route = url.parse(req.url).pathname
+  const match = router[`${route} ${method}`];
   // this is sloppy, espcially with more assets, create a "router"
-  if (route === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(findAsset('index.html'))
+
+  if(!match) {
+    res.writeHead(404)
+    logRequest(method, route, 404)
+    res.end()
+    return
+  }
+
+    res.writeHead(200, {'Content-Type': match.mime})
+    res.write(await findAsset(match.asset));
     logRequest(method, route, 200)
     res.end()
-  } else {
-    // missing asset should not cause server crash
-    throw new Error('route not found')
-    res.end()
-  }
   // most important part, send down the asset
 })
 
